da34a320920ac8ff05bed56c0aa639e9ba8d5691
Greg Goforth <greg.goforth@gmail.com>
2016-03-28 14:36:44 -0700

    actual app

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var express = require('express'),
+	router = express.Router(),
+	csv = require('csv'),
+	file = 'AutoSoftNET_SampleInventory.txt',
+	fs = require('fs'),
+	fc = fs.readFileSync(file, 'utf-8'),
+	title = 'AutoSoft csv parser';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	csv.parse(fc, {skip_empty_lines: true, relax: true, escape: true, delimiter: '|'}, function (err, _records) {
+		let columns = _records[0], //array with a single entry, pipe delimited
+			cars = _records.slice(1); //array of arrays
+
+		res.render('index', {columns, cars, title});
+	});
+});
+
+module.exports = router;
